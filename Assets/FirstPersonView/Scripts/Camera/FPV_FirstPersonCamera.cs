a786098ea20fcbe9ce9b5e31ad135b0610c496fa
Robby Sammelson <rsamm3@gmail.com>
2016-07-18 14:04:14 -0400

    first 3 assets

---
+++ Assets/FirstPersonView/Scripts/Camera/FPV_FirstPersonCamera.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace FirstPersonView
+{
+    public class FPV_FirstPersonCamera : FPV_Camera
+    {
+        void Awake()
+        {
+            SetCamera();
+            UpdateStaticCamera();
+        }
+
+        /// <summary>
+        /// Manualy update the static first person view camera variable.
+        /// </summary>
+        public override void UpdateStaticCamera()
+        {
+            FPV.firstPersonCamera = this;
+        }
+        
+        /// <summary>
+        /// Pre Cull Method.
+        /// Signal that the First Person Camera is going to start the rendering process
+        /// </summary>
+        void OnPreCull()
+        {
+            FPV_Renderer_Base.isWorldCameraRendering = false;
+            FPV_Renderer_Base.isFPVCameraRendering = true;
+        }
+
+        /// <summary>
+        /// On Post Render.
+        /// Signal that the First Person Camera is no longer rendering.
+        /// </summary>
+        void OnPostRender()
+        {
+            FPV_Renderer_Base.isFPVCameraRendering = false;
+        }
+    }
+}
---
+++ Assets/FirstPersonView/Scripts/Renderer/Base/IFPV_Renderer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace FirstPersonView
+{
+    /// <summary>
+    /// Interface for First Person Viewer Renderers.
+    /// </summary>
+    public interface IFPV_Renderer
+    {
+        /// <summary>
+        /// Setup the renderer
+        /// </summary>
+        /// <param name="render"></param>
+        void Setup(Renderer render, IFPV_Object parent);
+
+        /// </summary>
+        /// 
+        /// <summary>
+        void EnableFirstPersonViewer();
+
+        /// <summary>
+        /// Disable first Person Viewer.
+        /// </summary>
+        void DisableFirstPersonViewer();
+
+        /// <summary>
+        /// Set this renderer as First Person Object
+        /// </summary>
+        void SetAsFirstPersonObject();
+
+        /// <summary>
+        /// Set this renderer as First Person Object
+        /// </summary>
+        void RemoveAsFirstPersonObject();
+    }
+}
