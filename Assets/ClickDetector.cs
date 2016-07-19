3918363cdd07daf472e967ce43c6e13315844f30
PiGuy2 <rsammm3@gmail.com>
2016-07-19 10:49:22 -0400

    Raycast
    Removed FirstPersonView.
    Removed InstantGUI.
    Added script to Main Camera to detect what is being looked at.

---
+++ Assets/ClickDetector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickDetector : MonoBehaviour
+{
+	public bool wasHit = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		// Get the ray going through the GUI position
+		Vector2 v2ScreenCenter = new Vector2 (Screen.width / 2, Screen.height / 2);
+		// Vector3 v3ScreenCenter = new Vector3 (Screen.width / 2, Screen.height / 2, 0);
+		Ray ray = Camera.main.ScreenPointToRay (v2ScreenCenter);
+		// Do a raycast
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit)) {
+			wasHit = true;
+			print ("I'm looking at " + hit.transform.name);
+		} else {
+			wasHit = false;
+			print ("I'm looking at nothing!");
+		}
+	}
+}
